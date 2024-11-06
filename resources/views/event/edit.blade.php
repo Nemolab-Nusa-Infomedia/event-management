0d514dab86c2f20eba8f18f752ad3e75862e2826
RizqiA17 <asyriansyah@gmail.com>
2024-11-06 10:08:50 +0700

    view:edit event post migrate:fix nama status

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -7,22 +7,25 @@
 @stop
 
 @section('content')
-<form id="dataForm">
+<form id="dataForm" action="{{Route('event.update',$event->id)}}" method="POST">
+    @csrf
+    @method('PUT')
+
     <div class="mb-3 ">
         <label for="" class="form-label">Name</label>
         <input
-            type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" />
+            type="text" name="name" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{$event->name}}" />
         <label for="" class="form-label">Date</label>
         <input
-            type="date" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" />
+            type="date" name="event_date" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $event->event_date }}" />
         <label for="" class="form-label">Start</label>
         <input
-            type="time" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" />
+            type="time" name="event_start" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $event->event_start }}" />
         <label for="" class="form-label">End</label>
         <input
-            type="time" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" />
+            type="time" name="event_end" id="" class="form-control" placeholder="" aria-describedby="helpId" value="{{ $event->event_end }}" />
         <label for="" class="form-label">Location</label>
-        <textarea class="form-control" name="" id="" rows="3"></textarea>
+        <textarea class="form-control" name="location" id="" rows="3">{{ $event->location }}</textarea>
     </div>
     <button type="submit" class="btn btn-success mb-3"><i class="fas fa-plus"></i> Save</button>
     <a href="{{ route('event.index') }}" class="btn btn-danger mb-3"><i class="fas "><i class="fas fa-backward"></i> Back</a>
@@ -36,18 +39,6 @@
 
 @section('js')
 <script>
-    $(document).ready(function () {
-        $('#dataForm').on('submit', function () {
-            const url = "{{ route('event.update', $event->id) }}";
-            $.ajax({
-                type: "PUT",
-                url: "url",
-                data: $(this).serialize(),
-                success: function (response) {
-                    
-                }
-            });
-        });
-    });
+
 </script>
 @stop
