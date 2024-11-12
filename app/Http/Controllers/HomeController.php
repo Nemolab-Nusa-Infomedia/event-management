6d1849c7aa9d4d24a09ead40a6776c459102b934
RizqiA17 <asyriansyah@gmail.com>
2024-11-12 15:34:13 +0700

    controller: fix return data user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,21 +28,21 @@ class HomeController extends Controller
      */
     public function index()
     {
+
+        if (Auth::user()->role !== 'admin') {
+            $totalEvent = Events::where('id_master', '=', Auth::id())->count();
+            $eventAktif = Events::where('id_master', '=', Auth::id())->where('event_date', '=', now()->toDateString())->where('event_start', '<', now())->where('event_end', '>', now())->get()->count();
+            $eventSelesai = Events::where('id_master', '=', Auth::id())->where('event_date', '<=', now()->toDateString())->where('event_end', '<', now())->get()->count();
+            $event = Events::where('id_master', '=', Auth::id());
+            return view('home.index', compact('totalEvent',  'eventAktif', 'eventSelesai', 'event'));
+        }
+
         $totalEvent = Events::all()->count();
         $eventAktif = Events::where('event_date', '=', now()->toDateString())->where('event_start', '<', now())->where('event_end', '>', now())->get()->count();
         $eventSelesai = Events::where('event_date', '<=', now()->toDateString())->where('event_end', '<', now())->get()->count();
         $event = Events::all();
-        if (Auth::user()->role !== 'admin') {
-<<<<<<< Updated upstream
-            return view('home.index', compact('totalEvent', 'totalUser', 'eventAktif', 'eventSelesai', 'event'));
-        }
-;        return view('dashboard', compact(['totalEvent', 'totalUser', 'eventAktif', 'eventSelesai', 'event']));
-=======
-            return view('home.index', compact('totalEvent',  'eventAktif', 'eventSelesai', 'event'));
-        }
         $totalUser = User::all()->count();
         return view('dashboard', compact('totalEvent', 'totalUser', 'eventAktif', 'eventSelesai', 'event'));
->>>>>>> Stashed changes
     }
 
     public function Logout()
