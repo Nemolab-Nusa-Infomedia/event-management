9f78748214c32fe8840f0d7ee98e8978d386b9eb
RizqiA17 <asyriansyah@gmail.com>
2024-11-13 11:29:34 +0700

    view: tombol logout

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -11,60 +11,60 @@
         @csrf
         <div>
             <label for="name" class="block text-sm font-medium text-gray-700 mb-1">Name</label>
-            <input
-                type="text"
-                name="name"
-                id="name"
+            <input type="text" name="name" id="name" required
                 class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-                placeholder="Event Name"
-            />
+                placeholder="Event Name" value="{{ old('name') }}" />
+            @error('name')
+                <p class="text-red-600 text-xs font-semibold">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="event_date" class="block text-sm font-medium text-gray-700 mb-1">Date</label>
-            <input
-                type="date"
-                name="event_date"
-                id="event_date"
+            <input type="date" name="event_date" id="event_date" required
                 class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-            />
+                value="{{ old('event_date') }}" />
+            @error('event_date')
+                <p class="text-red-600 text-xs font-semibold">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="event_start" class="block text-sm font-medium text-gray-700 mb-1">Start</label>
-            <input
-                type="time"
-                name="event_start"
-                id="event_start"
+            <input type="time" name="event_start" id="event_start" required
                 class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-            />
+                value="{{ old('event_start') }}" />
+            @error('event_start')
+                <p class="text-red-600 text-xs font-semibold">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="event_end" class="block text-sm font-medium text-gray-700 mb-1">End</label>
-            <input
-                type="time"
-                name="event_end"
-                id="event_end"
+            <input type="time" name="event_end" id="event_end" required
                 class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-            />
+                value="{{ old('event_end') }}" />
+            @error('event_end')
+                <p class="text-red-600 text-xs font-semibold">{{ $message }}</p>
+            @enderror
         </div>
 
         <div>
             <label for="location" class="block text-sm font-medium text-gray-700 mb-1">Location</label>
-            <textarea
-                name="location"
-                id="location"
-                rows="3"
-                class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
-            ></textarea>
+            <textarea name="location" id="location" rows="3" required
+                class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm">{{ old('location') }}</textarea>
+            @error('location')
+                <p class="text-red-600 text-xs font-semibold">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="flex space-x-4">
-            <button type="submit" class="inline-flex items-center px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
+            <button type="submit"
+                class="inline-flex items-center px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
                 <i class="fas fa-plus mr-2"></i> Add Event
             </button>
-            <a href="{{ route('event.index') }}" class="inline-flex items-center px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
+            <a href="{{ route('event.index') }}"
+                class="inline-flex items-center px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
                 <i class="fas fa-backward mr-2"></i> Back
             </a>
         </div>
@@ -76,5 +76,7 @@
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <script>
+        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+    </script>
 @stop
