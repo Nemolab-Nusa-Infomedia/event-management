bb6a7384d9b067f04f96340fa7f5c4248ff6f334
RizqiA17 <asyriansyah@gmail.com>
2024-11-18 08:46:24 +0700

    Update navbar.blade.php

--- resources/views/layouts/components/landingpage/navbar.blade.php
+++ resources/views/layouts/components/landingpage/navbar.blade.php
@@ -31,7 +31,7 @@
                                     stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="text" name="email" id="topbar-search"
+                        <input type="search" name="search" id="topbar-search"
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-9 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             placeholder="Search">
                     </div>
