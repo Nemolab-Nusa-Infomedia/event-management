dcb15fcef2a9c42c18059e5cf22e16cb9faa3ea2
Deniss-RO <149313390+Deniss-RO@users.noreply.github.com>
2024-11-19 14:30:33 +0700

    event image, date & time update

--- resources/views/home/detailEvent.blade.php
+++ resources/views/home/detailEvent.blade.php
@@ -22,12 +22,12 @@
 </div>
 
 <section id="info" class="container mx-auto p-4 sm:p-16 md:p-24 mb-4 bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100" data-aos="fade-up" data-aos-offset="100">
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-12 items-start">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
         <div>
             <div class="p-4 sm:p-8 mb-6">
                 <h1 class="text-2xl sm:text-3xl dark:text-gray-200 font-bold text-gray-700 mb-4">Date & Time</h1>     
                 <div class="lg:flex">
-                    <p class="flex items-center">
+                    <p class="flex lg:border-r-2 pr-2 items-center">
                         <svg class="mx-2 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z" clip-rule="evenodd"/>
                         </svg>                      
@@ -67,7 +67,7 @@
             
             @for ($i = 0; $i < 4; $i++)
                 <img 
-                    class="transform transition duration-300 hover:scale-105 h-[250px] sm:h-[200px] md:h-[250px] lg:h-[250px] w-[250px] sm:w-[200px] md:w-[250px] lg:w-[350px] max-w-full rounded-lg" 
+                    class="transform transition duration-300 hover:scale-105 object-cover h-[250px] sm:h-[200px] md:h-[250px] lg:h-[250px] w-[250px] sm:w-[200px] md:w-[250px] lg:w-[350px] rounded-lg border border-gray-300" 
                     src="{{ isset($images[$i]) ? asset('storage/' . trim($images[$i])) : $placeholder }}" 
                     alt="Event Image {{ $i + 1 }}"
                 >
