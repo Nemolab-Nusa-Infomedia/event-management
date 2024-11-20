2708ee999b65f8e7ed27271e80d5cdab8bae6d33
Deniss-RO <149313390+Deniss-RO@users.noreply.github.com>
2024-11-20 15:48:41 +0700

    thumbnail fix

--- resources/views/home/detailEvent.blade.php
+++ resources/views/home/detailEvent.blade.php
@@ -3,18 +3,18 @@
 @section('content') 
 <div class="container mx-auto p-4 sm:p-8 mb-4">  
     <div class="w-full h-64 rounded-lg shadow-lg overflow-hidden mb-4" data-aos="fade-up" data-aos-offset="100">
-        <div class="scroll-container">
+        <div class="scroll-container w-full">
             <img src="{{ $event->thumbnail_img ? asset('storage/' . $event->thumbnail_img) : 'https://placehold.co/1700x1000/f3f4f6/000000/webp?text=Event+Image' }}"
             alt="{{ $event->name }}" 
                  alt="Event Image" 
-                 class="w-auto h-full object-cover">
+                 class="w-full h-full object-cover">
         </div>
     </div>
-    <p class="text-gray-500 dark:text-gray-300">
+    <p class="text-gray-500 dark:text-gray-300" data-aos="fade-right" data-aos-delay="100">
         <strong>{{ $formattedDate }}</strong>
     </p>
-    <h1 class="text-3xl sm:text-5xl dark:text-gray-200 font-bold text-gray-700 mb-8">{{ $event->name }}</h1>
-    <div class="flex items-center" data-aos="fade-up" data-aos-offset="100">
+    <h1 class="text-3xl sm:text-5xl dark:text-gray-200 font-bold text-gray-700 mb-8" data-aos="fade-right" data-aos-offset="100">{{ $event->name }}</h1>
+    <div class="flex items-center" data-aos="fade-right" data-aos-offset="100">
         <img class="w-8 h-8 rounded-full" src="{{ Auth::user()->profile_pict ? Storage::url('profile_pictures/' . Auth::user()->profile_pict) : 'https://flowbite.com/docs/images/people/profile-picture-5.jpg' }}"
         alt="user photo">
         <p class="mx-4">by <strong>{{ $creator->name }}</strong></p>
@@ -127,7 +127,7 @@
         white-space: nowrap;
     }
 
-    @media (min-width: 640px) {
+    @media (min-width: 1024px) {
     .scroll-container {
         animation: scroll-vertical 10s linear infinite alternate;
     }
