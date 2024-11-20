1e0cc9b8bdaf9777cae36d992dd2234ea397c442
RizqiA17 <asyriansyah@gmail.com>
2024-11-20 14:27:22 +0700

    fitur: tiket qr double

--- resources/views/home/joined.blade.php
+++ resources/views/home/joined.blade.php
@@ -41,8 +41,8 @@
                             {{ $item->event_end }}
                         </td>
                         <td class="px-6 py-4">
-                            <button data-modal-target="ticket-modal-{{ $item->id }}"
-                                data-modal-toggle="ticket-modal-{{ $item->id }}"
+                            <button data-modal-target="ticket-modal"
+                                data-modal-toggle="ticket-modal"
                                 data-qr-id="{{ $item->eventParticipants[0]->id }}"
                                 class="openTicket block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                 type="button">
@@ -61,8 +61,7 @@
     </div>
 
     <!-- Ticket modal -->
-    @foreach ($event as $item)
-        <div id="ticket-modal-{{ $item->id }}" tabindex="-1" aria-hidden="true"
+        <div id="ticket-modal" tabindex="-1" aria-hidden="true"
             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 bg-black/50 dark:bg-black/0 h-[calc(100%)] max-h-full">
             <div class="relative p-4 w-full max-w-2xl max-h-full">
                 <!-- Modal content -->
@@ -77,7 +76,7 @@
                         </div>
                         <button type="button"
                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-hide="ticket-modal-{{ $item->id }}" id="close-modal">
+                            data-modal-hide="ticket-modal" id="close-modal">
                             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                 viewBox="0 0 14 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -117,13 +116,12 @@
                     </div>
                     <!-- Modal footer -->
                     <div class="flex items-center p-4 md:p-5 rounded-b">
-                        <button data-modal-hide="ticket-modal-{{ $item->id }}" type="button"
+                        <button data-modal-hide="ticket-modal" type="button"
                             class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-gray-200 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Close</button>
                     </div>
                 </div>
             </div>
         </div>
-    @endforeach
 
     <script>
         let id = null;
