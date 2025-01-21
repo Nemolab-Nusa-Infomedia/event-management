3902dd63eca1136724c5d5c52ec240f9f8083ea6
vindrayulian <vindrayulian@gmail.com>
2025-01-21 09:24:17 +0700

    feat : change user

--- database/seeders/EventParticipantsSeeder.php
+++ database/seeders/EventParticipantsSeeder.php
@@ -10,26 +10,26 @@ class EventParticipantsSeeder extends Seeder
 {
     public function run(): void
     {
-        $events = Events::all();
-        $userIds = range(1, 10);
-        $participantIds = range(1, 10);
-        $statuses = ['Present', 'Absent'];
+        // $events = Events::all();
+        // $userIds = range(1, 10);
+        // $participantIds = range(1, 10);
+        // $statuses = ['Present', 'Absent'];
 
-        foreach ($events as $event) {
-            // For each event, create 3-7 participants
-            $numParticipants = rand(3, 7);
-            
-            for ($i = 0; $i < $numParticipants; $i++) {
-                // Randomly decide whether to use user_id or participant_id
-                $useUser = (bool)rand(0, 1);
-                
-                EventParticipants::create([
-                    'id_user' => $useUser ? $userIds[array_rand($userIds)] : null,
-                    'id_participant' => !$useUser ? $participantIds[array_rand($participantIds)] : null,
-                    'id_event' => $event->id,
-                    'status' => $statuses[array_rand($statuses)],
-                ]);
-            }
-        }
+        // foreach ($events as $event) {
+        //     // For each event, create 3-7 participants
+        //     $numParticipants = rand(3, 7);
+
+        //     for ($i = 0; $i < $numParticipants; $i++) {
+        //         // Randomly decide whether to use user_id or participant_id
+        //         $useUser = (bool)rand(0, 1);
+
+        //         EventParticipants::create([
+        //             'id_user' => $useUser ? $userIds[array_rand($userIds)] : null,
+        //             'id_participant' => !$useUser ? $participantIds[array_rand($participantIds)] : null,
+        //             'id_event' => $event->id,
+        //             'status' => $statuses[array_rand($statuses)],
+        //         ]);
+        //     }
+        // }
     }
 }
--- database/seeders/ParticipantSeeder.php
+++ database/seeders/ParticipantSeeder.php
@@ -9,13 +9,13 @@ class ParticipantSeeder extends Seeder
 {
     public function run(): void
     {
-        for ($i = 1; $i <= 10; $i++) {
-            Participants::create([
-                'name' => 'Participant ' . $i,
-                'email' => 'participant' . $i . '@example.com',
-                'no_telp' => '08' . rand(1000000000, 9999999999),
-                'alamat' => 'Jl. Participant ' . $i . ' No. ' . $i,
-            ]);
-        }
+        // for ($i = 1; $i <= 10; $i++) {
+        //     Participants::create([
+        //         'name' => 'Participant ' . $i,
+        //         'email' => 'participant' . $i . '@example.com',
+        //         'no_telp' => '08' . rand(1000000000, 9999999999),
+        //         'alamat' => 'Jl. Participant ' . $i . ' No. ' . $i,
+        //     ]);
+        // }
     }
 }
