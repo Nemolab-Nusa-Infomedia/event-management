30bb7f8e851f7da340d2be7ab5405e134f94c7e3
usss <88474883+sukimumuu@users.noreply.github.com>
2025-01-21 10:19:19 +0700

    fix: detail event

--- resources/views/home/detailEvent.blade.php
+++ resources/views/home/detailEvent.blade.php
@@ -184,7 +184,9 @@
                         @enderror
                     </div>
                     @csrf
+                    @auth
                     <input type="hidden" name="id_user" value="{{ Auth::user()->id }}">
+                    @endauth
                     <input type="hidden" name="id_event" value="{{ $event->id }}">
                     <button type="submit"
                         class="block mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
