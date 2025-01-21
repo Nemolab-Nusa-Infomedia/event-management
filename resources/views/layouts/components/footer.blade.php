12c3ae04ca15a1947fd63787de225007afe547f5
Deniss-RO <149313390+Deniss-RO@users.noreply.github.com>
2025-01-21 13:58:00 +0700

    link footer

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -2,6 +2,6 @@
     class="bg-white rounded-t-lg shadow-lg shadow-black dark:outline outline-gray-800 outline-2 dark:bg-gray-900 sm:ml-64 peer-checked:sm:ml-14">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a
-                href="https://flowbite.com/" class="hover:underline">Nemolab™</a>. All Rights Reserved.</span>
+                href="https://hugostudio.id/" class="hover:underline">HugoStudio™</a>. All Rights Reserved.</span>
     </div>
 </footer>
